a3ef4afa5f92c7f6e61ea6fb47a04202c2f279ed
Isaias Castillo <isaiascastillo@gmail.com>
2024-03-01 14:51:33 -0500

    Agrego los primero archivos del proyecto

---
+++ include/templates/footer.php
@@ -0,0 +1,23 @@
+<footer>
+<div class="navbar">
+            <a href="/">
+                <img class="navbar-logo" loading="lazy" src="build/img/JIC-LogoNaranjaBlanco2.png" alt="logo">
+            </a>
+            <a href="inicio.php">Inicio</a>
+            <a href="ejesPrincipios.php">Ejes y Principios</a>
+            <a href="propuestas.php">Propuestas</a>
+            <a href="conoceme.php">Conoceme</a>
+
+            <div class="redesSociales">
+            <a href="https://www.instagram.com/joseivancadogan/" target="_blank">
+                    <img class="logo-redes" loading="lazy" src="build/img/iconoInstagramBlanco.png" alt="logo">
+                </a>
+                <a href=""target="_blank">
+                    <img class="logo-redes" loading="lazy" src="build/img/iconoTwitterBlanco.png" alt="logo">
+                </a>
+                <a href=""target="_blank">
+                    <img class="logo-redes" loading="lazy" src="build/img/iconoFacebookBlanco.png" alt="logo">
+                </a>
+            </div>
+        </div>
+</footer>
