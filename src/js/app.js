7164b644269270e489f48642dd8cb7808f0b7b81
Isaias Castillo <isaiascastillo@gmail.com>
2024-03-08 17:41:44 -0500

    Agrego el codigo para el funcionamiento responsive del portafolio y terminado la primera version

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,25 @@
-var overlay = document.getElementById("overlay");
+const overlay = document.getElementById("overlay");
+const proyectos = document.getElementById("enlaceProyecto");
+const habilidades = document.getElementById("enlaceHabilidades");
+const contacto = document.getElementById("enlaceContacto");
+
+proyectos.addEventListener('click', function(evento) {
+  overlay.style.display = "none";
+  document.body.style.overflow = "auto"
+  
+});
+
+habilidades.addEventListener('click', function(evento) {
+  overlay.style.display = "none";
+  document.body.style.overflow = "auto"
+});
+
+contacto.addEventListener('click', function(evento) {
+  overlay.style.display = "none";
+  document.body.style.overflow = "auto"
+});
+
+
 
 function abrirMenu() {
   overlay.style.display = "block";
@@ -9,3 +30,20 @@ function cerrarMenu() {
   overlay.style.display = "none";
   document.body.style.overflow = "auto"
 }
+
+const emailInput = document.getElementById('emailInput');
+
+    // Agregar un detector de eventos para el clic en el campo de entrada
+    emailInput.addEventListener('click', function() {
+      // Obtener el valor del campo de entrada
+      const correoElectronico = emailInput.value;
+
+      // Verificar si se ingresó un correo electrónico
+      if (correoElectronico) {
+        // Crear el enlace mailto con el correo electrónico ingresado
+        const mailtoLink = `mailto:${correoElectronico}`;
+
+        // Abrir el cliente de correo electrónico predeterminado
+        window.open(mailtoLink);
+      }
+    });
